ae67d69cdd20f3a1f90b534e20a87b4ccd42adc1
dejandrea <dejandrea@gmail.com>
2023-03-20 19:05:50 -0300

    fase 2

--- sketch.js
+++ sketch.js
@@ -1,49 +1,49 @@
 //declarando as variáveis
-var trex, trexRunning
-var ground
-
+var trex, trexRunning;
+var ground;
+var groundImage;
+var ground2;
 //preload carrega as mídias do jogo
-function preload(){
+function preload() {
   //criando animação do trex correndo
-  trexRunning = loadAnimation("./images/trex3.png","./images/trex4.png")
+  trexRunning = loadAnimation("./images/trex3.png", "./images/trex4.png");
+  groundImage = loadImage("./images/ground2.png");
 }
 
-
 //setup faz a configuração
-function setup(){
-  createCanvas(600,200);
+function setup() {
+  createCanvas(600, 200);
 
   //sprite trex
-  trex = createSprite(50,160,20,50)
+  trex = createSprite(50, 160, 20, 50);
   //adcionando animação ao trex
-  trex.addAnimation("running",trexRunning)
-  trex.scale = 0.5
+  trex.addAnimation("running", trexRunning);
+  trex.scale = 0.5;
   //sprite Solo
-  ground = createSprite(300,190,600,20)
-  
-  //criando bordas
- 
+  ground = createSprite(300, 180, 600, 20);
+  ground.addImage(groundImage);
+  ground2 = createSprite(300, 190, 600, 10);
+  ground2.visible = false;
   
 }
 
 //draw faz o movimento, a ação do jogo
-function draw(){
+function draw() {
   background("lightgray");
 
   //pulo do trex
-  if (keyDown("space")) {
-    trex.velocityY = -10
+
+  if (keyDown("space") && trex.y > 160) {
+    trex.velocityY = -10;
   }
-
-  // gravidade
-  trex.velocityY += 0.5
-
-  //colisão com o solo
-  trex.collide(ground)
-
-
- 
+  trex.velocityY += 0.5;
+  trex.collide(ground2);
+  //console.log(trex.y)
+  ground.velocityX = -2;
+  if (ground.x < 0) {
+    ground.x = ground.width / 2;
+  }
   //coordenadas do mouse na tela
-  text("X: "+mouseX+" / Y: "+mouseY,mouseX,mouseY)
+  text("X: " + mouseX + " / Y: " + mouseY, mouseX, mouseY);
   drawSprites();
 }
